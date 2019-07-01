0ad1d29ac3ae594cab71a8fa98aff4d4e3810974
SetazeR <we.are@setazer.us>
2019-07-01 15:05:35 +0700

    tutorial 4

--- main/urls.py
+++ main/urls.py
@@ -13,13 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
+
+
 from . import views
 
 app_name = "main"
 
 urlpatterns = [
     path('', views.homepage, name="homepage"),
-    path(r'tinymce/', include('tinymce.urls')),
+
 ]
